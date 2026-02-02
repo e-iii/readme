568b39812e46a391689f0076d76588b6d2862360
ef <estrada.flsw@gmail.com>
2026-02-01 18:53:16 -0600

    core: config paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite"
+import path from "node:path"
 import vue from "@vitejs/plugin-vue"
 import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
-    plugins: [vue(), tailwindcss()]
+    plugins: [vue(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src")
+        }
+    }
 })
